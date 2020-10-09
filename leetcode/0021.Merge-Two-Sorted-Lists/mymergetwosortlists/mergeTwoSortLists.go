4d7fb4a60360cd95d29550cd2512f34b366dba2b
ybinemail <ybinemail@163.com>
2020-10-09 19:48:20 +0800

    commit

--- leetcode/0021.Merge-Two-Sorted-Lists/mymergetwosortlists/mergeTwoSortLists.go
+++ leetcode/0021.Merge-Two-Sorted-Lists/mymergetwosortlists/mergeTwoSortLists.go
@@ -6,11 +6,13 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	// 添加辅助头结点
 	head := &ListNode{}
 	head.Next = l1
 	p := head
 	q := l2
 	for p.Next != nil {
+		// 比较最小
 		if q != nil && q.Val <= p.Next.Val {
 			tmp := q
 			q = q.Next
@@ -20,6 +22,7 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 			p = p.Next
 		}
 	}
+	// 仍然有遗留
 	if q != nil {
 		p.Next = q
 	}
